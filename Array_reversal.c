e81939333552093f240a847331d14aa0eb3e3bf4
Priyanshu Sahu <priyanshusahu2611@gmail.com>
2025-04-06 00:42:34 +0530

    Add files via upload

---
+++ Array_reversal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+   int a[5];
+   printf("Enter array Element:");
+   for(int i =0 ;i<=4; i++){
+    scanf("%d",&a[i]);
+   }
+   printf("\nReverse array Elements:");
+   for (int i =4 ; i>=0 ; i--){
+   printf("%d", a[i]);
+   }
+   return 0;
+
+}
